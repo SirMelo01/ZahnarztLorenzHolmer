bcd510e12b105257b0ee9822397c70080d3bd207
Lennert Meißner <lennert.meissner@nordakademie.de>
2024-11-10 13:16:18 +0100

    Fixed Services and Hero Image Select

--- yoolink/static/js/cms/service/services.js
+++ yoolink/static/js/cms/service/services.js
@@ -7,16 +7,16 @@ $(document).ready(function() {
             $('#singleImageSection').hide();
 
             // Entfernt content-image von den Vergleichsbildern und fügt es dem Einzelbild hinzu
-            $('#sliderImageSection img').removeClass('content-image');
-            $('#singleImageSection img').addClass('content-image');
+            $('#sliderImageSection img').addClass('content-image');
+            $('#singleImageSection img').removeClass('content-image');
         } else {
             // Schaltet auf das Einzelbild um
             $('#sliderImageSection').hide();
             $('#singleImageSection').show();
 
             // Entfernt content-image vom Einzelbild und fügt es den Vergleichsbildern hinzu
-            $('#singleImageSection img').removeClass('content-image');
-            $('#sliderImageSection img').addClass('content-image');
+            $('#singleImageSection img').addClass('content-image');
+            $('#sliderImageSection img').removeClass('content-image');
         }
     });
 
@@ -24,12 +24,12 @@ $(document).ready(function() {
     if ($('#comparisionSwitch').is(':checked')) {
         $('#sliderImageSection').show();
         $('#singleImageSection').hide();
-        $('#sliderImageSection img').removeClass('content-image');
-        $('#singleImageSection img').addClass('content-image');
+        $('#sliderImageSection img').addClass('content-image');
+        $('#singleImageSection img').removeClass('content-image');
     } else {
         $('#sliderImageSection').hide();
         $('#singleImageSection').show();
-        $('#singleImageSection img').removeClass('content-image');
-        $('#sliderImageSection img').addClass('content-image');
+        $('#singleImageSection img').addClass('content-image');
+        $('#sliderImageSection img').removeClass('content-image');
     }
 });
