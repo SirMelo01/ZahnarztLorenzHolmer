a406965b3d4108913d1aa161b2fd0afa89c79c69
sr08678 <sebastian.rauch@stud.th-deg.de>
2024-11-11 23:07:59 +0100

    asdfas

--- yoolink/views.py
+++ yoolink/views.py
@@ -120,8 +120,10 @@ def impressum(request):
     user_settings = UserSettings.objects.filter(user__is_staff=False)
     if user_settings.exists():
         user_settings = user_settings.first()
+        adresse = user_settings.address
         context = {
-            'user_settings': user_settings
+            'user_settings': user_settings,
+            'adresse': adresse
         }
 
     return render(request, 'pages/impressum.html', context)
