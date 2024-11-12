e1a561a77fc4a5dea7d9767bef4cf4228d0ff452
Lennert Meißner <lennert.meissner@nordakademie.de>
2024-11-12 17:22:16 +0100

    Fixed Slider Removal

--- yoolink/static/js/cms/service/services.js
+++ yoolink/static/js/cms/service/services.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let originalAfterImageKey = $('#afterImage').attr('key');
     // Event Listener für den Switch
     $('#comparisionSwitch').on('change', function() {
         if ($(this).is(':checked')) {
@@ -9,6 +10,9 @@ $(document).ready(function() {
             // Entfernt content-image von den Vergleichsbildern und fügt es dem Einzelbild hinzu
             $('#sliderImageSection img').addClass('content-image');
             $('#singleImageSection img').removeClass('content-image');
+
+             // Setzt den Key von afterImage auf den ursprünglichen Key
+             $('#afterImage').attr('key', originalAfterImageKey);
         } else {
             // Schaltet auf das Einzelbild um
             $('#sliderImageSection').hide();
@@ -17,6 +21,9 @@ $(document).ready(function() {
             // Entfernt content-image vom Einzelbild und fügt es den Vergleichsbildern hinzu
             $('#singleImageSection img').addClass('content-image');
             $('#sliderImageSection img').removeClass('content-image');
+
+            // Setzt den Key von afterImage auf "nothing"
+            $('#afterImage').attr('key', 'nothing');
         }
     });
 
@@ -26,10 +33,12 @@ $(document).ready(function() {
         $('#singleImageSection').hide();
         $('#sliderImageSection img').addClass('content-image');
         $('#singleImageSection img').removeClass('content-image');
+        $('#afterImage').attr('key', originalAfterImageKey);
     } else {
         $('#sliderImageSection').hide();
         $('#singleImageSection').show();
         $('#singleImageSection img').addClass('content-image');
         $('#sliderImageSection img').removeClass('content-image');
+        $('#afterImage').attr('key', 'nothing');
     }
 });
