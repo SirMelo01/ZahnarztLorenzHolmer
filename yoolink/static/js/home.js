3900f8ef9bcd95cd93cc7598a4843275eaaf143a
Lennert Meißner <lennert.meissner@nordakademie.de>
2024-11-12 23:35:05 +0100

    Corrected it agaIN

--- yoolink/static/js/home.js
+++ yoolink/static/js/home.js
@@ -6,37 +6,37 @@
 $(document).ready(function() {
   var csrfTokenInput = document.querySelector('input[name="csrfmiddlewaretoken"]');
   var csrfToken = csrfTokenInput ? csrfTokenInput.value : undefined;
-  /**
-   * Email Form submit Function (index page)
-   * How to use: Compare wukschweiss project
-   */
+
   $('#emailForm').submit(function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    var formData = {
-        name: $('#name').val(),
-        email: $('#email').val(),
-        title: $('#subject').val(),
-        message: $('#message').val(),
-        csrfmiddlewaretoken: csrfToken,
-    };
-    // Send form data to the server using AJAX
-    $.ajax({
-        type: 'POST',
-        url: '/cms/email/request/',
-        data: formData,
-        success: function(response) {
-            // Handle successful response here
-            if(response.success) {
-              sendNotif("Ihre Nachricht wurde erfolgreich gesendet", "success")
-            }
-            $('#emailForm')[0].reset();
-        },
-        error: function(xhr, status, error) {
-            // Handle error response here
-            sendNotif("Etwas ist schief gelaufen. Versuchen Sie es bitte später nochmal.", "error")
-        }
-    });
+      event.preventDefault(); // Prevent the default form submission
+
+      // Updated formData to match field IDs
+      var formData = {
+          name: $('#name').val(),
+          email: $('#email').val(),
+          title: $('#title').val(),  // Corrected to match HTML ID
+          message: $('#message').val(),
+          csrfmiddlewaretoken: csrfToken,
+      };
+
+      // Send form data to the server using AJAX
+      $.ajax({
+          type: 'POST',
+          url: '/cms/email/request/',
+          data: formData,
+          success: function(response) {
+              if (response.success) {
+                  sendNotif("Ihre Nachricht wurde erfolgreich gesendet", "success");
+              }
+              $('#emailForm')[0].reset();
+          },
+          error: function(xhr, status, error) {
+              sendNotif("Etwas ist schief gelaufen. Versuchen Sie es bitte später nochmal.", "error");
+          }
+      });
+  });
 });
+
 
 /*setTimeout(() => {
   if (cookiemapselect !== null && cookiemapselect !== "false") {
